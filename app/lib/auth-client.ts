3c5411b4fe1e56a1be3f9cd9f53d1f9eb6ec72c0
Sohrab <sohrab@postliter8.com>
2026-04-12 22:12:52 +0200

    removed redundant files

--- app/lib/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from "better-auth/client"
-export const authClient =  createAuthClient()
-
-const signIn = async () => {
-    const data = await authClient.signIn.social({
-        provider: "linkedin"
-    })
-}
