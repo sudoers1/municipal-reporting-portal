45678ce2c6a458344dca5888337aab11d4629304
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-12 22:19:23 +0200

    Move lib folder from app into the root

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { auth } from "../lib/auth";
+import { auth } from "../../lib/auth";
 import { headers } from "next/headers";
 
 export async function signUpAction(formData: FormData){
