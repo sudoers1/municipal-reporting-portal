1ad36efd7f5b3e979072f18a1e96a0010b66aaa9
husain-battey <2595252@students.wits.ac.za>
2026-04-12 18:52:42 +0200

    updated directory structure

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,9 +4,9 @@ import Database from "better-sqlite3";
 export const auth = betterAuth({
   database: new Database("./database.db"),
   socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID!,    // 70557103088-3mlrhdagdp00foasfsccmq45oenah1i6.apps.googleusercontent.com
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
     },
   },
 });
