8026cb56b5af0387cf8a4a45950681683f6dec6b
Sohrab <sohrab@postliter8.com>
2026-04-21 20:51:38 +0200

    remove async function from client page (complaintsTable)

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,9 +1,19 @@
 import { readComplaints } from "@/lib/db/complaints";
 import ComplaintsTable from "@/components/complaintsTable";
+import { useState, useEffect } from "react";
 
-export default async function Reports() {
-  
-  const complaints = await readComplaints();
+export default function Reports() {
+
+  const [complaints, setComplaints] = useState<Record<string, any>[]>([]);
+
+  useEffect(() => {
+    async function getComplaints() {
+      return await readComplaints();
+    }
+
+    const c = getComplaints();
+    setComplaints(prev => [...prev, c]);
+  }, []);
 
   return (
     <main
@@ -11,19 +21,19 @@ export default async function Reports() {
       style={{ backgroundImage: "url('/municipality.png')" }}
     >
       <section className="p-8 space-y-10 md:bg-black/50 min-h-[120vh] flex flex-col">
-        
+
         <header>
           <h1 className="text-3xl md:text-5xl font-bold text-white text-center">
             View All Reports
           </h1>
         </header>
 
-        
+
 
         <figure className="flex md:justify-center">
           <ComplaintsTable complaints={complaints} />
         </figure>
-        
+
 
       </section>
     </main>
