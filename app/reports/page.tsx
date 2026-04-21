85fb0a8aa15af99e2d14be2314b93e08e99b6feb
husain-battey <2595252@students.wits.ac.za>
2026-04-22 00:15:43 +0200

    fixed display errors on reports page

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -7,16 +7,33 @@ import { useState, useEffect } from "react";
 export default function Reports() {
 
   const [complaints, setComplaints] = useState<Record<string, any>[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getComplaints() {
-      return await readComplaints();
+      const data= await readComplaints();
+      setComplaints(data);
+      setLoading(false);
     }
 
-    const c = getComplaints();
-    setComplaints(prev => [...prev, c]);
+    getComplaints();
+    
   }, []);
 
+  if(loading){return (
+    <main
+      className="w-screen min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/municipality.png')" }}
+    >
+      <section className="p-8 bg-black/50 min-h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center gap-4">
+          <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin" />
+          <p className="text-white text-lg font-semibold">Loading Reports...</p>
+        </div>
+      </section>
+    </main>
+  );}
+  else{
   return (
     <main
       className="w-screen min-h-[120vh] overflow-y-auto bg-cover bg-center bg-no-repeat"
@@ -39,5 +56,5 @@ export default function Reports() {
 
       </section>
     </main>
-  );
+  );}
 }
