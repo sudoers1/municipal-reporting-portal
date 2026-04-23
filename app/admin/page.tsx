3894c753363ea63d4263f5bf4afe478ca52f97b9
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-23 21:55:24 +0200

    Created AdminTiles.tsx and edited app/admin/page.tsx
    - Created AdminTiles component
    - Added some styling to AdminTiles
    - Linked admin tiles to the page.tsx of app/admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import Tiles from "@/components/Dashboard/AdminTiles";
 import AdminLayout from "../../layouts/AdminLayout";
 
 
@@ -5,12 +6,13 @@ export default function AdminPage() {
   return (
     <>
     <AdminLayout/>
-    <main className="m-4">
+    <main className="m-12 flex flex-col justify-center items-center">
       <section>
         <h1 className="text-6xl pb-4">Welcome Admin</h1>
         <p className="text-xl">Some informative text for the admin to read</p>
       </section>
       <section>
+        <Tiles/>
       </section>
     </main>
     </>
