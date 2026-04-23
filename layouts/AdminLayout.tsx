bd73259588703053beef02db271a5930ae564847
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-23 18:31:24 +0200

    Created the basic layout for the admin view
    The layout has a navigation bar
    A section for the main content of the page
    And a footer
    * I will add a menu for the navigation at a later stage, I am still deciding if I want a regular menu, hamburger or side bar menu

--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -1,12 +1,18 @@
 export default function AdminLayout(){
     return(
         <>
-        <header>
-            <div>Muncipal Portal Project</div>
+        <header className="bg-gray-400 p-2 flex justify-between">
+            <div className="text-2xl flex justify-center align-items ">Muncipal Portal Project</div>
             <section>
-                <div className=""></div>
+                <div className="bg-gray-300 border-2 border-gray-600 w-8 h-8 rounded-4xl"></div>
             </section>
         </header>
+        <main>
+            {/* all the main content will go in here */}
+        </main>
+        <footer className="bg-gray-400 w-full h-8 p-2 flex flex-col justify-center items-center fixed bottom-0 ">
+            © 2026 sudoers1. All rights reserved.
+        </footer>
         </>
     )
 }
