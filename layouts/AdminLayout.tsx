2915b5ff40c108259491f16333a625419721fea2
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-23 17:44:00 +0200

    File Structure Intialised
    - Component folders created
    - Layout for Admin page created
    - Created some folders in App

---
+++ layouts/AdminLayout.tsx
@@ -0,0 +1,12 @@
+export default function AdminLayout(){
+    return(
+        <>
+        <header>
+            <div>Muncipal Portal Project</div>
+            <section>
+                <div className=""></div>
+            </section>
+        </header>
+        </>
+    )
+}
