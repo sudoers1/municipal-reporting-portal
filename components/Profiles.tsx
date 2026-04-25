048c9d1057f87b49dc4c4af235fa878d4fbd33e8
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-25 13:59:49 +0200

    Fixed some issues with the profiles modal
    - Added a background colour to the profiles modal so that it's not see-through
    - Const a state variable for closing the modal
    - The Modal can be closed by clicking the X button and the logout button.
    -  Removed div tags
    - Added a modal to tap on the user icon to view the user details

--- components/Profiles.tsx
+++ components/Profiles.tsx
@@ -1,12 +1,24 @@
+import React from "react";
+
 export default function Profiles(){
+
+    const [showModal, setShowModal] = React.useState(true);
+
+    if(!showModal) return null;
+
     return(
-        <section className="absolute top-full right-4 shadow-xl z-50">
+        <section className="bg-gray-100 absolute top-full right-4 shadow-xl z-50">
             <section className="border w-100 p-4 relative">
-                <button className="absolute top-2 right-4 ">X</button>
-                <div className="py-4 flex flex-col justify-center items-center gap-2">
-                    <div className="bg-gray-400 w-24 h-24 border-2 border-gray-600 rounded-full"></div>
+                <button 
+                    onClick={() => setShowModal(false)} 
+                    className="bg-red-600 text-white px-2 absolute top-2 right-4 border border-transparent rounded"
+                >
+                    Close
+                </button>
+                <section className="py-4 flex flex-col justify-center items-center gap-2">
+                    <section className="bg-gray-400 w-24 h-24 border-2 border-gray-600 rounded-full"></section>
                     <h2>Admin Name</h2>
-                </div>
+                </section>
                 <section className="flex flex-col justify-center items-center gap-2">
                     <p>admin@email.address</p>
                     <p>admin phone number</p>
@@ -14,7 +26,7 @@ export default function Profiles(){
                 </section>
                 <section className="py-4 flex flex-col justify-center items-center gap-4">
                     <h3 className="bg-gray-200 text-center w-24 h-8 flex justify-center items-center rounded-2xl">Admin</h3>
-                    <button className="bg-gray-200 text-center w-24 h-8 flex justify-center items-center rounded-md"><a href="">Logout</a></button>
+                    <button  className="bg-gray-200 text-center w-24 h-8 flex justify-center items-center rounded-md"><a href="">Logout</a></button>
                 </section>
             </section>
         </section>
--- layouts/AdminLayout.tsx
+++ layouts/AdminLayout.tsx
@@ -1,10 +1,23 @@
+import Profiles from "@/components/Profiles";
+import React from "react";
+
 export default function AdminLayout(){
+
+    const [showModal, setShowModal] = React.useState(false);
+
+    
+
     return(
         <>
         <header className="bg-gray-400 p-2 flex justify-between relative">
-            <div className="text-2xl flex justify-center align-items ">Muncipal Portal Project</div>
+            <section className="text-2xl flex justify-center align-items ">Muncipal Portal Project</section>
             <section>
-                <div className="bg-gray-300 border-2 border-gray-600 w-8 h-8 rounded-4xl"></div>
+                <section 
+                    onClick={()=> setShowModal(true)} 
+                    className="bg-gray-300 border-2 border-gray-600 w-8 h-8 rounded-4xl"
+                >
+                </section>
+                {showModal && <Profiles/>}
             </section>
         </header>
         <main>
