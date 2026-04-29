13219b24ba5d78a825d56770ed8ca529e645b95a
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-29 20:07:16 +0200

    Create a popup with complaint details
    - Popup modal that will have details about the complaint from the database

--- components/Forms/ComplaintDetails.tsx
+++ components/Forms/ComplaintDetails.tsx
@@ -1,7 +1,18 @@
 export default function ComplaintDetails(){
     return(
-        <section>
-            <h1></h1>
+        <section className="min-w-80 p-4 border border-gray-600">
+            <h1 className="font-semibold text-2xl text-center p-2">Complaint No.001</h1>
+            <section className="p-4 flex flex-col gap-2">
+                <p className="">Complaint Details...</p>
+                <p className="">Complaint Details...</p>
+                <p className="">Complaint Details...</p>
+                <p className="">Complaint Details...</p>
+                <p className="">Complaint Details...</p>
+                <p className="">Complaint Details...</p>
+            </section>
+            <section className="flex justify-center">
+                <button className=" bg-gray-300 px-4 py-2 m-2 rounded-md">Allocate Work</button>
+            </section>
         </section>
     )
 }
