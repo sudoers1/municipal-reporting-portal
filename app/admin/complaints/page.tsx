7ec54428d1cb282d0c220210f8f8ab5df7e82531
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-30 14:56:52 +0200

    Created a back button

--- app/admin/complaints/page.tsx
+++ app/admin/complaints/page.tsx
@@ -1,4 +1,5 @@
 import TaskAllocationForm from "@/components/Forms/TaskAllocationForm";
+import BackButton from "@/components/Tools/BackButton";
 import ComplaintsTable from "@/components/Tools/CompTable";
 import SearchBar from "@/components/Tools/SearchBar";
 import AdminLayout from "@/layouts/AdminLayout";
@@ -11,7 +12,8 @@ export default function ComplaintsPage(){
     return(
         <>
         <AdminLayout/>
-        <main>
+        <BackButton/>
+        <main className="p-2 flex flex-col gap-8">
             <SearchBar/>
             <section className="flex justify-center relative">
                 <button onClick={() => setShowModal(true)} className=" bg-gray-300 w-fit px-4 py-2 rounded-md">Allocate Tasks To Workers</button>
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,13 @@
 import Tiles from "@/components/Dashboard/AdminTiles";
 import AdminLayout from "../../layouts/AdminLayout";
+import BackButton from "@/components/Tools/BackButton";
 
 
 export default function AdminPage() {
   return (
     <>
     <AdminLayout/>
+    <BackButton/>
     <main className="m-12 flex flex-col justify-center items-center">
       <section>
         <h1 className="text-6xl pb-4 max-sm:text-5xl">Welcome Admin</h1>
