501a9f910b942b271c44bc895798731ad359bbeb
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-04-30 20:05:36 +0200

    Created Admin dashboard view
    - Connected the dashboard page to the button on the admin landing page
    - Created the KPICards
    - Created the box for the interactive map and the status legend

---
+++ components/Dashboard/KPIcard.tsx
@@ -0,0 +1,11 @@
+export default function KPICards(){
+    return(
+        <section className="m-4 grid grid-cols-5 gap-4">
+            <section className="bg-slate-300 h-30 rounded-md"></section>
+            <section className="bg-slate-300 h-30 rounded-md"></section>
+            <section className="bg-slate-300 h-30 rounded-md"></section>
+            <section className="bg-slate-300 h-30 rounded-md"></section>
+            <section className="bg-slate-300 h-30 rounded-md"></section>
+        </section>
+    )
+}
---
+++ components/Dashboard/StatusLegend.tsx
@@ -0,0 +1,7 @@
+export default function StatusLegend(){
+    return(
+        <section className="h-100 flex justify-center items-center rounded-md border">
+            <h1 className="text-2xl">Some extra information will be stored here</h1>
+        </section>
+    )
+}
