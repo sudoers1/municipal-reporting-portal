5e32dd5300d058d905a4b9566e17f0fe2a68ef44
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-05-05 20:52:53 +0200

    Created KPI Cards with data

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -4,8 +4,12 @@ import MapView from "@/components/Map/MapView";
 import BackButton from "@/components/Tools/BackButton";
 import SearchBar from "@/components/Tools/SearchBar";
 import AdminLayout from "@/layouts/AdminLayout";
+import { readComplaints } from "../../../lib/db/complaints";
 
-export default function AdminDashboard(){
+export default async function AdminDashboard(){
+
+    const complaints = await readComplaints();
+
     return(
         <>
             <AdminLayout/>
@@ -18,7 +22,7 @@ export default function AdminDashboard(){
                 </section>
             </section>
             <main>
-                <KPICards/>
+                <KPICards data={complaints}/>
                 <section className="p-4 grid grid-cols-2 gap-8">
                     <MapView/>
                     <StatusLegend/>
