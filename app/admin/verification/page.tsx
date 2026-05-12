d63ac7b76e096affb369a51f6b6b42dcde3c343c
nakairuzive-student01 <nakii.ruzie@gmail.com>
2026-05-12 15:38:42 +0200

    IInitialised verification page connection

---
+++ app/admin/verification/page.tsx
@@ -0,0 +1,8 @@
+export default function Verification(){
+    return(
+        <main className="text-center">
+            <h1>User Verification</h1>
+            <p>Review user applications to become workers or admins on the system.</p>
+        </main>
+    )
+}
--- components/Dashboard/AdminTiles.tsx
+++ components/Dashboard/AdminTiles.tsx
@@ -13,7 +13,7 @@ export default function Tiles(){
                 <h2 className="font-semibold text-3xl">Manage</h2>
                 <p className="text-sm">Oversee your team by managing system users and field workers to ensure resources are effectively distributed across all active projects.</p>
                 <button className="bg-brand-secondary/50 px-4 py-1 rounded-md shadow-sm"><a href="">Users</a></button>
-                <button className="bg-brand-secondary/50 px-4 py-1 rounded-md shadow-sm"><a href="">workers</a></button>
+                <button className="bg-brand-secondary/50 px-4 py-1 rounded-md shadow-sm"><a href="/admin/verification">workers</a></button>
             </section>
         </main>
     )
